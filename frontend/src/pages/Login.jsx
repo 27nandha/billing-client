b89c23d433cdedaaa4063b341e726a46d8a882b5
27nandha <nandhakumar272002@gmail.com>
2025-07-29 04:55:30 +0530

    ngrok

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { Link, Navigate } from "react-router-dom";
-import { use, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
@@ -15,9 +15,11 @@ const Login = () => {
   const [auth, setAuth] = useAuth();
 
   // Redirect if already logged in
-  if (auth && auth.user && auth.token) {
-    return <Navigate to="/" replace />;
-  }
+  useEffect(() => {
+    if (auth && auth.user && auth.token) {
+      navigate("/", { replace: true });
+    }
+  }, [auth]);
 
   const [loading, setLoading] = useState(false);
 
